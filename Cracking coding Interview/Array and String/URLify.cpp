36a930acc4a7298ca31b3bd8266c230616449520
jatin verma <42065404+jv640@users.noreply.github.com>
2020-09-10 11:52:26 +0530

    Create URLify.cpp

---
+++ Cracking coding Interview/Array and String/URLify.cpp
@@ -0,0 +1,53 @@
+/*
+  Description : Write a method to replace all spaces in a string with '%20: You may assume that the string has sufficient space at the end to hold the additional characters, 
+                and that you are given the "true" length of the string/
+*/
+int replaceSpaces(char str[]) 
+{ 
+    // count spaces and find current length 
+    int space_count = 0, i; 
+    for (i = 0; str[i]; i++) 
+        if (str[i] == ' ') 
+            space_count++; 
+  
+    // Remove trailing spaces 
+    while (str[i-1] == ' ') 
+    { 
+       space_count--; 
+       i--; 
+    } 
+  
+    // Find new length. 
+    int new_length = i + space_count * 2 + 1; 
+  
+    // New length must be smaller than length 
+    // of string provided. 
+    if (new_length > MAX) 
+        return -1; 
+  
+    // Start filling character from end 
+    int index = new_length - 1; 
+  
+    // Fill string termination. 
+    str[index--] = '\0'; 
+  
+    // Fill rest of the string from end 
+    for (int j=i-1; j>=0; j--) 
+    { 
+        // inserts %20 in place of space 
+        if (str[j] == ' ') 
+        { 
+            str[index] = '0'; 
+            str[index - 1] = '2'; 
+            str[index - 2] = '%'; 
+            index = index - 3; 
+        } 
+        else
+        { 
+            str[index] = str[j]; 
+            index--; 
+        } 
+    } 
+  
+    return new_length; 
+} 
