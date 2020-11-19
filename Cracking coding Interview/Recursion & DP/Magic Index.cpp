f4d700241c3140f73647d399d979783af4b3bae1
jatin verma <42065404+jv640@users.noreply.github.com>
2020-11-19 11:18:04 +0530

    Update Magic Index.cpp

--- Cracking coding Interview/Recursion & DP/Magic Index.cpp
+++ Cracking coding Interview/Recursion & DP/Magic Index.cpp
@@ -6,16 +6,16 @@
 // Code
 int magicindex(vector<int> A){
   if (start <= end) {
-			int mid = (start + end) / 2;
-			if (mid == A[mid]) // check for magic index.
-				return mid;
-			if (mid > A[mid]) { // If mid>A[mid] means fixed point might be on
-								// the right half of the array
-				return search(A, mid + 1, end);
-			} else {// If mid<A[mid] means fixed point might be on
-				// the left half of the array
-				return search(A, start, mid - 1);
-			}
-		}
-		return -1;
+	int mid = (start + end) / 2;
+	if (mid == A[mid]) // check for magic index.
+		return mid;
+	if (mid > A[mid]) { // If mid>A[mid] means fixed point might be on
+						// the right half of the array
+		return search(A, mid + 1, end);
+	} else {// If mid<A[mid] means fixed point might be on
+		// the left half of the array
+		return search(A, start, mid - 1);
+	}
+   }
+   return -1;
 }
